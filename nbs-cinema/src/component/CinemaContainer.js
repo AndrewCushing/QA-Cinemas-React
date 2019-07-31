4b6b2b8306bbd46792af28def30f697050fd1d59
AndrewCushing <andyc.bt4567@gmail.com>
2019-07-31 12:50:31 +0100

    Revert "Revert "Merge pull request #3 from AndrewCushing/revert-1-Sprint1-Carl""
    This reverts commit d55bafd1d5fb70979c623947d9ace1f91254df03.

--- nbs-cinema/src/component/CinemaContainer.js
+++ nbs-cinema/src/component/CinemaContainer.js
@@ -22,29 +22,26 @@ import Scrum from './ScrumDescription'
 import Standard from './StandardScreen'
 import NewReleases from './UpcomingFilms'
 
-
 class LunaCinemaApp extends Component {
     render() {
         return (
             <>
-           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
                 <Router>
                     <>
                         <HeaderComponent />
-                       
                         <div className="container">
                             <Switch>
                                 <Route path="/" exact component={HomeComponent} />
                                 <Route path="/home" component={HomeComponent} />
                                 <Route path="/about" exact component={About} />
-                                <Route path="/Booking/:id" component={Booking} />
+                                <Route path="/Booking" component={Booking} />
                                 <Route path="/classifications" component={Classification} />
                                 <Route path="/contact" component={Contact} />
                                 <Route path="/CurrentFilms" component={CurrentFilms} />
                                 <Route path="/DeluxeScreen" component={Deluxe} />
                                 <Route path="/DiscussionBoard" component={Discussion} />
                                 <Route path="/email" component={EMail} />
-                                <Route path="/FilmDetails/:id" component={FilmDetails} />
+                                <Route path="/FilmDetails" component={FilmDetails} />
                                 <Route path="/travel" component={GettingThere} />
                                 <Route path="/OpeningTimes" component={Opening} />
                                 <Route path="/payment" component={Payment} />
