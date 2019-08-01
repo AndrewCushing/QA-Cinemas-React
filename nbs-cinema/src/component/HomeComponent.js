a9dc8754750d0c3ac16f9c9a68bcb5d442a20355
JaminSun <b.tinson@hotmail.com>
2019-08-01 15:09:46 +0100

    Home Component updated

--- nbs-cinema/src/component/HomeComponent.js
+++ nbs-cinema/src/component/HomeComponent.js
@@ -3,56 +3,13 @@ import Carousel from 'react-bootstrap/Carousel';
 import './HomeComponent.css';
 
 
-class CurrentFilms extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-        console.log("This is my initializer")
-        const movies = [
-            {id: 0, film_src: "ConAir.jpg",  title: "Con Air"},
-            {id: 1, film_src: "GrandBH.jpg",  title: "Grand Budapest Hotel"}
-        ]
-        const films =  movies.map((F) =>
-            ( <film_src key={F.id} title={F.title} />
-            ));
-        return(
-            <div>
-                {films}
-            </div>
-        )
-    };
-}
-
 
 class HomeComponent extends React.Component {
-    state = {
-    };
-    componentDidMount() {
-        // HomeService.executeGetService()
-        //     .then(response => {
-        //         this.setState({
-        //             homes: response.data
-        //         })
-        //     })
-    };
-    // gotoBeanComponent = () => {
-    //     this.props.history.push('/add-home')
-    // }
-
-    // deleteHome = (id) => {
-    //     HomeService.executeDeleteService(id)
-    //         .then(response => {
-    //             this.setState({
-    //                 homes: response.data
-    //             })
-    //         })
-    // }
-
     render() {
         return (
             <div>
                 <h1> Welcome to Luna Cinemas </h1>
-                <h5> “Life is not the amount of breaths you take. It’s the moments that take your breath away.” </h5>
+                <h5> “One tiny hop for bird, one giant flight for cinema” </h5>
                 <Carousel
                     className={"carousel"}
                     controls = {true}
@@ -60,13 +17,12 @@ class HomeComponent extends React.Component {
                     interval = {3000}
                     wrap = {true}
                     indicators = {false}
+                    >
 
-
-                >
                     <Carousel.Item>
                         <img
                             className="CaroImg"
-                            src="/images/ConAir.jpg"
+                            src="/images/CurrentFilmImages/Lionking.jpg"
                             alt="Slide Coming Soon"
                         />
 
@@ -74,32 +30,58 @@ class HomeComponent extends React.Component {
                     <Carousel.Item>
                         <img
                             className="CaroImg"
-                            src="/images/GrandBH.jpg"
+                            src="/images/CurrentFilmImages/Spider.jpg"
                             alt="Slide Coming Soon"
                         />
                     </Carousel.Item>
-
 
                     <Carousel.Item>
                         <img
                             className="CaroImg"
-                            src="/images/babydriver.jpg"
+                            src="/images/CurrentFilmImages/Toystory.jpg"
                             alt="Slide Coming Soon"
                         />
                     </Carousel.Item>
 
+                    <Carousel.Item>
+                        <img
+                            className="CaroImg"
+                            src="/images/CurrentFilmImages/Yesterday.jpg"
+                            alt="Slide Coming Soon"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="CaroImg"
+                            src="/images/UpcomingFilmsImages/GeminiMan.png"
+                            alt="Slide Coming Soon"
+                        />
 
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="CaroImg"
+                            src="/images/UpcomingFilmsImages/Goodboys.jpg"
+                            alt="Slide Coming Soon"
+                        />
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img
+                            className="CaroImg"
+                            src="/images/UpcomingFilmsImages/Jumanji.jpg"
+                            alt="Slide Coming Soon"
+                        />
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <img
+                            className="CaroImg"
+                            src="/images/UpcomingFilmsImages/Zombieland2.png"
+                            alt="Slide Coming Soon"
+                        />
+                    </Carousel.Item>
                 </Carousel>
-                {/*<div>*/}
-
-                {/*    <ul className = "list-group">*/}
-                {/*        {this.state.home.map(home => <li className = "list-group-item" key={home.id}>home: {home.homeName} <button className="btn btn-danger float-right"onClick={() => {*/}
-                {/*            this.deletehome(home.id)}}>x</button></li>)}*/}
-                {/*    </ul>*/}
-                {/*</div>*/}
-                {/*<div className="float-right">*/}
-                {/*    <button className="btn btn-success " onClick={this.gotoBeanComponent}>Add home</button>*/}
-                {/*</div>*/}
             </div>
         )
     }
