18f0c6ccb2a8abe5a3749a39afcfa20740ef4eef
NoNegs <carlosmomo@hotmail.co.uk>
2019-08-05 09:48:55 +0100

    Footer Updated

--- nbs-cinema/src/component/Footer.js
+++ nbs-cinema/src/component/Footer.js
@@ -1,63 +1,103 @@
-import React from "react";
+import React, { Component } from "react";
+import { MDBCol,MDBIcon, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import styled from 'styled-components';
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Styles = styled.div`
 .page-footer{
     background:#22465E;
 }
-  `;
 
-const Footer = () => {
-  return (
-      <Styles>
-        <MDBFooter color="blue" className="font-small pt-4 mt-4">
-          <MDBContainer fluid className="text-center text-md-left">
-            <MDBRow>
-              <MDBCol md="6">
-                <h5 className="title5">Luna Cinema's</h5>
-                <p>
-                </p>
-              </MDBCol>
-              <MDBCol md="6">
-                <h5 className="title">Cinema Information</h5>
-                <ul>
-                  <li className="list-unstyled">
-                    <a href="/About">About Us</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/Classification">Classification</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/Contact">Contact Us</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/EMail">Email Form</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/GettingThere">Directions</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/PlacesToGo">Places To Go</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/Scrum">Scrum</a>
-                  </li>
-                  <li className="list-unstyled">
-                    <a href="/DiscussionBoard">Discussion Board</a>
-                  </li>
-                </ul>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-          <div className="footer-copyright text-center py-3">
-            <MDBContainer fluid>
-              &copy; {new Date().getFullYear()} Copyright: Luna Cinema's
-            </MDBContainer>
-          </div>
-        </MDBFooter>
-      </Styles>
-  );
+.FooterText{
+    color: antiquewhite;  
 }
 
-export default Footer;
+.mb-4 {
+    font-style:normal;
+    color: antiquewhite;
+    font-size:12px;
+}
+.ul.list-unstyled  {
+    font-style:normal;
+    color: antiquewhite;
+    
+}
+  `;
+
+
+export default class AppFooter extends Component {
+  render(){
+    return (
+        <Styles>
+          <MDBFooter color=" pt-4 mt-4">
+            <MDBContainer className="text-center text-md-left">
+              <MDBRow className="my-4">
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="2" lg="2" className="ml-auto">
+                  <h5 className="mb-4 font-weight-bold"  style={{ textDecoration: "underline"}}>About</h5>
+                  <ul className="list-unstyled">
+                    <p>
+                      <a className="FooterText" href="/About">About Us</a>
+                    </p>
+                    <p>
+                      <a className="FooterText" href="/Contact">Contact Us</a>
+                    </p>
+                    <p>
+                      <a className="FooterText" href="/Scrum">Scrum Description</a>
+                    </p>
+                    <p>
+                      <a className="FooterText"href="/DiscussionBoard">Discussion Board</a>
+                    </p>
+                  </ul>
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="2" lg="2" className="ml-auto">
+                  <h5 className="mb-4 font-weight-bold"  style={{ textDecoration: "underline"}}>Cinema Information</h5>
+                  <ul className="list-unstyled">
+                    <p>
+                      <a className="FooterText" href="/Classifications">Classification</a>
+                    </p>
+                    <p>
+                      <a className="FooterText" href="/Directions">Directions</a>
+                    </p>
+                    <p>
+                      <a className="FooterText" href="/PlacesToGo">Places To Go</a>
+                    </p>
+                  </ul>
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="5" lg="3">
+                  <h5  className="mb-4 font-weight-bold" style={{ textDecoration: "underline"}}>Address</h5>
+                  <p className="FooterText">
+                    <i className="fa fa-home mr-3" />Alexandra House, Wroughton, Swindon SN4 0QJ
+                  </p>
+                  <p className="FooterText">
+                    <i className="fa fa-phone mr-3" /> +44 1234 567890
+                  </p>
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="2" lg="2" className="text-center">
+                  <h5 className="mb-4 font-weight-bold"  style={{ textDecoration: "underline"}}>
+                    Follow us
+                  </h5>
+                  <div className="mt-2 ">
+                    <a className="btn btn-link" href="#!">
+                      <MDBIcon className="text-white" style ={{paddingLeft:"10px"}}size = "2x" icon="facebook" />
+                    </a>
+                    <a className="btn btn-link" href="">
+                      <MDBIcon className="text-white" style ={{paddingLeft:"10px"}}size = "2x" icon="twitter" />
+                    </a>
+                  </div>
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+              </MDBRow>
+            </MDBContainer>
+            <div className="footer-copyright text-center py-3">
+              <MDBContainer fluid className="FooterText" >
+                &copy; {new Date().getFullYear()} Luna Cinema's
+              </MDBContainer>
+            </div>
+          </MDBFooter>
+        </Styles>
+    );
+  }
+}
