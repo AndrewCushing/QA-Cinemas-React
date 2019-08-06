31d8139625c28dc9995f7fc521b48e21e4a60c4c
NoNegs <carlosmomo@hotmail.co.uk>
2019-08-06 16:03:58 +0100

    Created Jumbotrons for additional pages and corrected NavBar

--- nbs-cinema/src/component/HomeComponent.js
+++ nbs-cinema/src/component/HomeComponent.js
@@ -8,25 +8,9 @@ class HomeComponent extends Component {
     state = {
     };
     componentDidMount() {
-        // HomeService.executeGetService()
-        //     .then(response => {
-        //         this.setState({
-        //             homes: response.data
-        //         })
-        //     })
-    };
-    // gotoBeanComponent = () => {
-    //     this.props.history.push('/add-home')
-    // }
 
-    // deleteHome = (id) => {
-    //     HomeService.executeDeleteService(id)
-    //         .then(response => {
-    //             this.setState({
-    //                 homes: response.data
-    //             })
-    //         })
-    // }
+    };
+
 
     render() {
         return (
@@ -68,16 +52,6 @@ class HomeComponent extends Component {
 
 
                 </Carousel>
-                {/*<div>*/}
-
-                {/*    <ul className = "list-group">*/}
-                {/*        {this.state.home.map(home => <li className = "list-group-item" key={home.id}>home: {home.homeName} <button className="btn btn-danger float-right"onClick={() => {*/}
-                {/*            this.deletehome(home.id)}}>x</button></li>)}*/}
-                {/*    </ul>*/}
-                {/*</div>*/}
-                {/*<div className="float-right">*/}
-                {/*    <button className="btn btn-success " onClick={this.gotoBeanComponent}>Add home</button>*/}
-                {/*</div>*/}
             </div>
         )
     }
--- nbs-cinema/src/component/NavigationBar.js
+++ nbs-cinema/src/component/NavigationBar.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import {
-    MDBNavbar, MDBNavbarBrand,MDBIcon, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
-    MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
-} from "mdbreact";
+    MDBNavbar, MDBNavbarBrand,MDBIcon, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline} from "mdbreact";
 import styled from 'styled-components';
 const Logo = '/qacinemalogo.png'
 
